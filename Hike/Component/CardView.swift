ae9732b6d5911a313b4953abfe75c676211b5bce
saphitv <simonmaggini@gmail.com>
2023-10-22 12:24:57 +0200

    added new component

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        // MARK: - CARD
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
-                        startPoint: .topLeading, endPoint: .bottomTrailing
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
+                            startPoint: .topLeading, endPoint: .bottomTrailing
+                        )
                     )
-                )
-                .frame(width: 256, height: 256)
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+                    .frame(width: 256, height: 256)
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
