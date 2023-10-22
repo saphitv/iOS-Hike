ddf60eb27c54711c66132904f3d7fb23212f295d
saphitv <simonmaggini@gmail.com>
2023-10-22 12:23:07 +0200

    init repository

---
+++ Hike/Component/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Simon Maggini on 22.10.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
+                        startPoint: .topLeading, endPoint: .bottomTrailing
+                    )
+                )
+                .frame(width: 256, height: 256)
+            Image("image-1")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Hike/Utility/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Simon Maggini on 22.10.2023.
+//
+
+import SwiftUI
+
+struct ColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorExtension()
+}
